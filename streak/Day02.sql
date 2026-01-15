678fb125421228b4608a97bb35bc1bea955b9ef3
Anish Chandlekar <163227286+AnishGitFlow@users.noreply.github.com>
2026-01-15 20:05:06 +0530

    Add files via upload

---
+++ streak/Day02.sql
@@ -0,0 +1,31 @@
+-- Day 02
+
+create table emp (
+  emp_id INT,
+  emp_name VARCHAR(10),
+  salary INT,
+  manager_id INT
+)
+
+INSERT INTO emp VALUES (1,'Ankit',10000,4);
+INSERT INTO emp VALUES (2,'Mohit',15000,5);
+INSERT INTO emp VALUES (3,'Vikas',10000,4);
+INSERT INTO emp VALUES (4,'Rohit',5000,2);
+INSERT INTO emp VALUES (5,'Mudit',12000,6);
+INSERT INTO emp VALUES (6,'Agam',12000,2);
+INSERT INTO emp VALUES (7,'Sanjay',9000,2);
+INSERT INTO emp VALUES (8,'Ashish',5000,2);
+
+
+-- Solution
+
+SELECT
+    e.emp_id,
+    e.emp_name,
+    m.emp_name AS manager_name,
+    e.salary,
+    m.salary AS manager_salary
+FROM emp e
+INNER JOIN emp m
+    ON e.manager_id = m.emp_id
+WHERE e.salary > m.salary;
